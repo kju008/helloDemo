0e480c0b34be1b960de93d7238a00d5d729ce5ec
smailiy <puthathat@nate.com>
2022-02-15 14:24:06 +0900

    init

---
+++ helloDemo/src/test/java/kr/ibksystem/helloDemo/HelloDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ibksystem.helloDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
